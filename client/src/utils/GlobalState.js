c36da496cffebf381b3b0bf8214dcf3c27adc0a5
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-18 17:49:31 -0800

    Upload code to be refractored

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { useProductReducer } from "./reducers";
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        cart: [],
+        cartOpen: false,
+        categories: [],
+        currentCategory: "",
+    });
+
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
