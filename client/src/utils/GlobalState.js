42b61cab8170703fe60e0651d2c989abf4af388b
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-18 19:25:21 -0800

    Update Global state for redux

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,24 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { useProductReducer } from "./reducers";
+import store from "./store";
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
-
-const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-        products: [],
-        cart: [],
-        cartOpen: false,
-        categories: [],
-        currentCategory: "",
-    });
-
-    return <Provider value={[state, dispatch]} {...props} />;
+export const StoreProvider = (props) => {
+    return <Provider store={store} {...props} />;
 };
-
-const useStoreContext = () => {
-    return useContext(StoreContext);
-};
-
-export { StoreProvider, useStoreContext };
