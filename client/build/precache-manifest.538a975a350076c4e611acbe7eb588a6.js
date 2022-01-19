69f968aeb6f473bace14a5ca69ca9785185b34c0
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-19 14:04:37 -0800

    deploying

---
+++ client/build/precache-manifest.538a975a350076c4e611acbe7eb588a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06626ab65a250e1582d0576b7ebfb86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dfbefde871c037009e5",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "3ae115219b160df5a582",
+    "url": "/static/js/2.d105b8a0.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.d105b8a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dfbefde871c037009e5",
+    "url": "/static/js/main.57d1a8b2.chunk.js"
+  },
+  {
+    "revision": "93b95f33a1daad010cdd",
+    "url": "/static/js/runtime-main.33e5d76d.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
